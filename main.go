6a4c689a3ea8723f2d90802198d98170b6b9c849
nirutk <nirutk@vgi.co.th>
2025-04-18 20:29:17 +0700

    add structur to keep decode data to model

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"go-tamboon/cipher"
 	"io"
 	"os"
+	"strconv"
 )
 
 type Donation struct {
@@ -45,4 +47,56 @@ func main() {
 	}
 
 	fmt.Println("Decryption successful! Output file:", outputFile)
+
+	donations, err := readDonations(outputFile)
+	if err != nil {
+		fmt.Println("Error reading donations:", err)
+		return
+	}
+
+	for _, d := range donations {
+		fmt.Printf("%+v\n", d)
+	}
+}
+
+func readDonations(csvPath string) ([]Donation, error) {
+	file, err := os.Open(csvPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	// ensures the CSV has exactly 6 fields per line
+	reader.FieldsPerRecord = 6
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var donations []Donation
+	for i, record := range records {
+		if i == 0 {
+			// skip header
+			continue
+		}
+
+		amount, _ := strconv.Atoi(record[1])
+		expMonth, _ := strconv.Atoi(record[4])
+		expYear, _ := strconv.Atoi(record[5])
+
+		d := Donation{
+			Name:           record[0],
+			AmountSubunits: amount,
+			CCNumber:       record[2],
+			CVV:            record[3],
+			ExpMonth:       expMonth,
+			ExpYear:        expYear,
+		}
+
+		donations = append(donations, d)
+	}
+
+	return donations, nil
 }
