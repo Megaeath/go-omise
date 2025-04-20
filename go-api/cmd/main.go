86404c9b9f37885b4bbaf828196c9a6a57de9e25
Auii <auii@Auiis-MacBook-Air.local>
2025-04-20 21:42:08 +0700

    add monitoring

--- go-api/cmd/main.go
+++ go-api/cmd/main.go
@@ -4,14 +4,33 @@ import (
 	"go-api/internal/config"
 	"go-api/internal/router"
 	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	requestCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"path"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(requestCount)
+}
 
 func main() {
 	config.InitRedis()
 	config.InitMongo()
 	config.InitKafkaTopic()
-	
+
 	r := router.SetupRouter()
+	http.Handle("/metrics", promhttp.Handler())
 
 	log.Println("API running at :8080")
 	if err := r.Run(":8080"); err != nil {
--- go-worker/cmd/main.go
+++ go-worker/cmd/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"go-worker/internal/consumer"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -21,7 +24,10 @@ func main() {
 	if group == "" {
 		group = "charge-consumer"
 	}
-
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":2112", nil)
+	}()
 	workers := 10
 	log.Println("Starting charge worker...")
 	err := consumer.StartKafkaConsumer(broker, topic, group, workers)
